8460be7986f4263c4001e69be4c1e878d041955b
prabhat kumar <115355888+Prabhatkumar999@users.noreply.github.com>
2023-07-09 01:35:08 +0530

    Add files via upload

---
+++ ATM-Simulator-System/src/ASimulatorSystem/Conn.java
@@ -0,0 +1,21 @@
+package ASimulatorSystem;
+
+import java.sql.*;  
+
+public class Conn{
+	
+    Connection c;
+    Statement s;
+    
+    public Conn(){  
+        try{
+        	
+            c = DriverManager.getConnection("jdbc:mysql:///bankmanagementsystem", "root", "Prabhat@1999");    
+            s = c.createStatement();
+            
+            
+        } catch (Exception e) { 
+            System.out.println(e);
+        }  
+    }  
+}  
